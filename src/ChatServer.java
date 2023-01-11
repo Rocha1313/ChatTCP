febef918cb4a8ee74b393a590e7f99f95b9ae306
rocha <rjoao0112@gmail.com>
2023-01-11 14:45:21 +0000

    Exercise Done

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class ChatServer {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatServer {
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(8086);
+        Socket clientSocket = serverSocket.accept();
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        while (true) {
+            String inputLine = in.readLine();
+            if (inputLine == null) {
+                break;
+            }
+            out.println("Server: " + inputLine);
+        }
+    }
 }
